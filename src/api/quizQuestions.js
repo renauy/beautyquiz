a09570f565a1f10ceefc2b144dc018ca401bda75
SERENA CLARISSE UY <serenauy@SERENAs-MacBook.local>
2019-04-24 17:32:18 +0800

    first commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,110 @@
+var quizQuestions = [
+    {
+        question: "How do your pores generally look like?",
+        answers: [
+            {
+                type: "Oily",
+                content: "Large and visible"
+            },
+            {
+                type: "Normal",
+                content: "Unnoticable"
+            },
+            {
+              type: "Dry",
+              content: "Small pores that feel tight"
+            },
+            {
+                type: "Combination",
+                content: "Medium sized and mainly visible in T-zone"
+            }
+        ]
+    },
+    {
+        question: "How does your skin typically feel?",
+        answers: [
+            {
+                type: "Oily",
+                content: "Greasy and needs to be blotted throughout day"
+            },
+            {
+                type: "Normal",
+                content: "Not too dry or oil and is not that sensitive"
+            },
+            {
+              type: "Dry",
+              content: "Feels dry and tight especially during winter"
+            },
+            {
+                type: "Combination",
+                content: "Feels oily in T-zone area but dry everywhere else"
+            }
+        ]
+    },
+    {
+        question: "How does your skin feel after cleansing?",
+        answers: [
+            {
+                type: "Oily",
+                content: "Feels clean and oil-free but oiliness returns soon after"
+            },
+            {
+                type: "Normal",
+                content: "Feels clean and not greasy/dry throughout day"
+            },
+            {
+                type: "Dry",
+                content: "Feels tight and dry and in need of moisturizer"
+            },
+  
+            {
+                type: "Combination",
+                content: "Oil-free around nose area but dry around cheek area"
+            }
+        ]
+    },
+    {
+        question: "How often do you need to moisturize?",
+        answers: [
+            {
+                type: "Oily",
+                content: "Once throughout day as skin stays oily throughout"
+            },
+            {
+                type: "Normal",
+                content: "Just once throughout day as skin stays fairly hydrated"
+            },
+            {
+              type: "Dry",
+              content: "Immediately after cleansing skin and throughout day"
+            },
+            {
+                type: "Combination",
+                content: "Usually around cheek area but not around T-zone area"
+            }
+        ]
+    },
+    {
+        question: "What type of moisturizer do you usually use?",
+        answers: [
+            {
+                type: "Oily",
+                content: "Oil-free or gel-based"
+            },
+            {
+                type: "Normal",
+                content: "Any moisturizer will do"
+            },
+            {
+                type: "Dry",
+                content: "Rich and typically fragrance-free"
+            },
+            {
+                type: "Combination",
+                content: "Gel-based"
+            }
+        ]
+    }
+  ];
+
+export default quizQuestions;
